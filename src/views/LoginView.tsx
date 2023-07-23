7709954105b86476e039e3c510701a6c973164f8
Joonas Köppä <joonas.koppa@smartly.io>
2023-07-23 02:45:09 +0200

    use generic header and footer

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,16 +1,19 @@
 import { ReactElement, useEffect } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default function LoginView(): ReactElement {
   return (
-    <div className="w-full px-4 py-5 sm:p-6 content-center">
-      <h1 className="text-amber-400 text-3xl">Promoteur</h1>
-      <div className="mt-3">
+    <div className="w-full px-4 py-5 sm:p-6 content-center flex flex-col">
+      <Header />
+      <div className="mt-8 flex justify-center">
         <div className="connect-button">
           <ConnectButton />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
