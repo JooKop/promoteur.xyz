14dc3aabd44ca23c2670ac7b5bbb6e0631edcc7f
Joonas Köppä <joonas.koppa@smartly.io>
2023-07-23 02:44:16 +0200

    generic header

---
+++ src/views/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header(): ReactElement {
+  return (
+    <div className="mt-8 flex flex-row justify-center">
+      <img className="w-16" src="megaphone.png" />
+      <img className="w-96" src="promoteur.png" />
+    </div>
+  );
+}
